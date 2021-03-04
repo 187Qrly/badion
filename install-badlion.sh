e2abd4ccb892686c1c1e83530876b732e2da8d8e
187Qrly <56300140+187Qrly@users.noreply.github.com>
2021-03-04 21:19:19 +0100

    Update install-badlion.sh

--- install-badlion.sh
+++ install-badlion.sh
@@ -1,5 +1,8 @@
 echo "installing the Badlion Client..."
 cp BadlionClient /usr/bin/
 cp badlion.desktop /usr/share/applications
+chmod a+x /usr/bin/BadlionClient
+chmod a+x /usr/share/applications/badlion.desktop
+
 echo "should be installed now. Thanks for using the Installer & watching my video :)"
 
