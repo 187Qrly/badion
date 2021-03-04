6080a7a74f02c4b08fbca62b38dc09c60b28758f
187Qrly <56300140+187Qrly@users.noreply.github.com>
2021-03-04 20:35:16 +0100

    Add files via upload

---
+++ install-badlion.sh
@@ -0,0 +1,5 @@
+echo "installing the Badlion Client..."
+cp BadlionClient /usr/bin/
+cp BadlionClient.desktop /usr/share/applications
+echo "should be installed now. Thanks for using the Installer & watching my video :)"
+
